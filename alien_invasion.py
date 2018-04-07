1776013ec0b1c43071e17922b34de8a9a737628a
Jerry Zhu <atongmu518@gmail.com>
2018-04-07 21:18:56 +0800

    放置外星飞船到屏幕询问下方

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,13 +3,18 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
     ai_settings = Settings()
+
+
     screen = pygame.display.set_mode((
         ai_settings.screen_width, ai_settings.screen_height))
+    ship = Ship(screen)
+
     pygame.display.set_caption("外星人入侵")
 
     # 开始游戏的主循环
@@ -21,7 +26,8 @@ def run_game():
 
         # 每次循环都重绘屏幕
         screen.fill(ai_settings.bg_color)
-
+        ship.blitme()
+        
         #让最近绘制的屏幕可见
         pygame.display.flip()
 
